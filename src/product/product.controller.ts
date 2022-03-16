65fab6acf46e921a16a1c212d67b6edd26a2ff38
Kevin <hellokevinvogel@gmail.com>
2022-03-16 08:35:59 +0100

    chore(product): improve product svc

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,29 +1,35 @@
 import { Controller, Get, Inject, OnModuleInit, Param, ParseIntPipe, UseGuards, Post, Body } from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
-import { FindOneResponse, ProductServiceClient, PRODUCT_SERVICE_NAME, CreateResponse, CreateRequest } from './product.pb';
+import {
+  FindOneResponse,
+  ProductServiceClient,
+  PRODUCT_SERVICE_NAME,
+  CreateProductResponse,
+  CreateProductRequest,
+} from './product.pb';
 import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('product')
 export class ProductController implements OnModuleInit {
-  private service: ProductServiceClient;
+  private svc: ProductServiceClient;
 
   @Inject(PRODUCT_SERVICE_NAME)
   private readonly client: ClientGrpc;
 
   public onModuleInit(): void {
-    this.service = this.client.getService<ProductServiceClient>(PRODUCT_SERVICE_NAME);
+    this.svc = this.client.getService<ProductServiceClient>(PRODUCT_SERVICE_NAME);
   }
 
   @Post()
   @UseGuards(AuthGuard)
-  private async getStock(@Body() body: CreateRequest): Promise<Observable<CreateResponse>> {
-    return this.service.create(body);
+  private async createProduct(@Body() body: CreateProductRequest): Promise<Observable<CreateProductResponse>> {
+    return this.svc.createProduct(body);
   }
 
   @Get(':id')
   @UseGuards(AuthGuard)
   private async findOne(@Param('id', ParseIntPipe) id: number): Promise<Observable<FindOneResponse>> {
-    return this.service.findOne({ id });
+    return this.svc.findOne({ id });
   }
 }
--- src/product/product.pb.ts
+++ src/product/product.pb.ts
@@ -24,14 +24,14 @@ export interface FindOneResponse {
   data: FindOneData | undefined;
 }
 
-export interface CreateRequest {
+export interface CreateProductRequest {
   name: string;
   sku: string;
   stock: number;
   price: number;
 }
 
-export interface CreateResponse {
+export interface CreateProductResponse {
   status: number;
   error: string[];
   id: number;
@@ -42,18 +42,20 @@ export const PRODUCT_PACKAGE_NAME = 'product';
 export interface ProductServiceClient {
   findOne(request: FindOneRequest): Observable<FindOneResponse>;
 
-  create(request: CreateRequest): Observable<CreateResponse>;
+  createProduct(request: CreateProductRequest): Observable<CreateProductResponse>;
 }
 
 export interface ProductServiceController {
   findOne(request: FindOneRequest): Promise<FindOneResponse> | Observable<FindOneResponse> | FindOneResponse;
 
-  create(request: CreateRequest): Promise<CreateResponse> | Observable<CreateResponse> | CreateResponse;
+  createProduct(
+    request: CreateProductRequest,
+  ): Promise<CreateProductResponse> | Observable<CreateProductResponse> | CreateProductResponse;
 }
 
 export function ProductServiceControllerMethods() {
   return function (constructor: Function) {
-    const grpcMethods: string[] = ['findOne', 'create'];
+    const grpcMethods: string[] = ['findOne', 'createProduct'];
     for (const method of grpcMethods) {
       const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
       GrpcMethod('ProductService', method)(constructor.prototype[method], method, descriptor);
